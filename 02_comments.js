9fadad75fcf6e52a191e4eedc5976dc541de410a
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2019-12-09 11:48:18 +0200

    Comments exercise

---
+++ 02_comments.js
@@ -0,0 +1,5 @@
+// 1. Write a single line comment which says, comments can make code readable
+// comments can make code readable
+
+// 2. Write a multiline comment which says, comments can make code readable, easy to use and informative
+/* comments can make code readable, easy to use and informative */
