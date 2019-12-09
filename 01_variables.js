f34f5c1cf5f813bb0dcdebd8d7cbed73744e2148
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2019-12-09 11:40:15 +0200

    Exercise variables

---
+++ 01_variables.js
@@ -0,0 +1,29 @@
+// Exercise 1 Variable
+//1. Declare four variables without assigning values
+
+let a;
+let b;
+let c;
+let d;
+
+// 2. Declare four variables with assigning values
+let numString = '1';
+let numTwo = 2;
+let numThree = 9;
+let bool = true;
+
+// 3. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+const firstName = 'Anna';
+const lastName = 'Riazantseva';
+let marStatus = 'married';
+let homeCountry = 'Russia';
+let age = 27;
+
+// 4. Declare variables to store your first name, last name, marital status, country and age in a single line
+const myString = `My name is ${firstName}. My lastname is ${lastName}. I'm ${marStatus}. I'm ${homeCountry}. I'm ${age} years old`;
+
+// 5. Declare two variables myAge and yourAge and assign them initial values and log to browser console. Output:
+let myAge = 12;
+let yourAge = 30;
+
+console.log(`${myAge} ${yourAge}`);
