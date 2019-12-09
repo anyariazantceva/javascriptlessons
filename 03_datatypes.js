e7ca217be79d3a0c554a92a2b3893ce16890047b
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2019-12-09 14:37:28 +0200

    Booleans and datatypes exercises

---
+++ 03_datatypes.js
@@ -0,0 +1,9 @@
+let newStr = 'Hello';
+let newBool = true;
+let a;
+let b = null;
+
+console.log(typeof newStr);
+console.log(typeof newBool);
+console.log(typeof a);
+console.log(typeof b);
---
+++ 05_booleans.js
@@ -0,0 +1,9 @@
+// 1. Write three JavaScript statement which provide truthy value.
+let isRaining = true;
+let isOpened = true;
+let isSnowing = true;
+
+// 2. Write three JavaScript statement which provide falsy value.
+let hungry = false;
+let isSick = false;
+let newTeacher = false;
